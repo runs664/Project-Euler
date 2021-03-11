b9e47502018c17b53b7728c770ceb5fe2f30acd0
runs-15 <harunsixsixfour@gmail.com>
2021-03-11 13:19:14 +0700

    005

--- Python/005 Smallest Multiple.py
+++ Python/005 Smallest Multiple.py
@@ -4,6 +4,8 @@ Project Euler's Problem 005
 2520 adalah bilangan terkecil yang dapat habis dibagi oleh semua angka dari 1 sampai 10.
 Berapakah bilangan positif terkecil yang dapat habis dibagi oleh semua bilangan dari 1 sampai 20?
 """
+import time
+s = time.time()
 i = 9699690 # merupakan hasil perkalian dari seluruh faktor prima antara 1 - 20, karena pasti lebih besar dari ini
 while True:
     for x in range(1, 21):
@@ -13,4 +15,5 @@ while True:
         print(i)
         break
     i += 1
+print('Operation completed in', round(time.time() - s, 2), 'second')
 # PS: Ini model pencarian secara brute force, sehingga memerlukan waktu cukup lama (+- 2 menit)
